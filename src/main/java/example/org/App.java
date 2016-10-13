d5c78d78d234cf044254cf664306981b0b230749
Martin Czygan <martin.czygan@gmail.com>
2016-10-13 16:27:36 +0200

    add minimal action

--- src/main/java/example/org/App.java
+++ src/main/java/example/org/App.java
@@ -1,5 +1,7 @@
 package example.org;
 
+import org.apache.commons.lang3.math.Fraction;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(Fraction.FOUR_FIFTHS);
     }
 }
