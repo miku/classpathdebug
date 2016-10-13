4ecfc281d2b5e9f7d6033ff38f3a33904abceb5f
Martin Czygan <martin.czygan@gmail.com>
2016-10-13 16:38:56 +0200

    add another library, still ok

--- src/main/java/example/org/App.java
+++ src/main/java/example/org/App.java
@@ -1,6 +1,7 @@
 package example.org;
 
 import org.apache.commons.lang3.math.Fraction;
+import org.apache.commons.math3.complex.Complex;
 
 /**
  * Hello world!
@@ -11,5 +12,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(Fraction.FOUR_FIFTHS);
+        System.out.println(Complex.I);
     }
 }
